6d0b17df000bab615903d92818d086f8a5eab5d4
NachiketInamdar <ni_inamdar@yahoo.com>
2026-01-19 06:31:02 +0530

    Created .NET Core app and added unit tests.

---
+++ tests/MyApp.UnitTests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿namespace MyApp.UnitTests;
+using MyApp.Services;
+using Xunit;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsException()
+    {
+        var calc = new Calculator();
+        Assert.Throws<ArgumentException>(() => calc.Divide(10, 0));
+    }
+}
