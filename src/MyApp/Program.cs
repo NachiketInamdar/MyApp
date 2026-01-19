72613ccb9084d73552984ff433ca8ca1d61627b1
NachiketInamdar <ni_inamdar@yahoo.com>
2026-01-19 15:08:53 +0530

    Testing branch policies again

--- src/MyApp/Program.cs
+++ src/MyApp/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("MyApp started successfully");
-Console.WriteLine("Branch policies");
+Console.WriteLine("Branch policies 2");
